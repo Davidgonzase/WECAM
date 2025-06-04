c6094017c2702d010820f3edc2639a1db51fa1bb
David Gonzalez <94617139+Davidgonzase@users.noreply.github.com>
2025-06-04 22:07:03 +0200

    Add files via upload

--- WECAMWEB/islands/video.tsx
+++ WECAMWEB/islands/video.tsx
@@ -3,14 +3,13 @@ import { FunctionComponent } from "preact";
 type Context = {
   jwt: string;
   id: string;
-  name: string;
+  name: string
 };
 
 export const Stream: FunctionComponent<Context> = (props) => {
   const scriptContent = `
     const status = document.getElementById("status");
     const videoElement = document.getElementById("remoteVideo");
-    const fullscreenButton = document.getElementById("fullscreenButton");
 
     const peerConnection = new RTCPeerConnection({
       iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
@@ -25,7 +24,7 @@ export const Stream: FunctionComponent<Context> = (props) => {
     async function fetchOffer() {
       try {
         console.log("Inicializando")
-        const response = await fetch("http://localhost:8010/camera", {
+        const response = await fetch("https://wecam.onrender.com/camera", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -58,7 +57,7 @@ export const Stream: FunctionComponent<Context> = (props) => {
 
           console.log("sdp", peerConnection.localDescription.sdp)
 
-          await fetch("http://localhost:8010/putsdp", {
+          await fetch("https://wecam.onrender.com/putsdp", {
             method: "PUT",
             headers: {
               "Content-Type": "application/json",
@@ -80,27 +79,6 @@ export const Stream: FunctionComponent<Context> = (props) => {
     }
 
     fetchOffer();
-
-    // Fullscreen functionality
-    fullscreenButton.addEventListener("click", () => {
-      if (!document.fullscreenElement) {
-        if (videoElement.requestFullscreen) {
-          videoElement.requestFullscreen();
-        } else if (videoElement.webkitRequestFullscreen) {
-          videoElement.webkitRequestFullscreen();
-        } else if (videoElement.msRequestFullscreen) {
-          videoElement.msRequestFullscreen();
-        }
-      } else {
-        if (document.exitFullscreen) {
-          document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) {
-          document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) {
-          document.msExitFullscreen();
-        }
-      }
-    });
   `;
 
   return (
@@ -108,7 +86,6 @@ export const Stream: FunctionComponent<Context> = (props) => {
       <h1>Stream {props.name}</h1>
       <video id="remoteVideo" autoplay playsinline></video>
       <p id="status">Initializing...</p>
-      <button id="fullscreenButton">Pantalla Completa</button>
       <script dangerouslySetInnerHTML={{ __html: scriptContent }}></script>
     </div>
   );
